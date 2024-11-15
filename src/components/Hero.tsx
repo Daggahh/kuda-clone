7cbfec660be862f7417ab2ad4b85a350c7752848
Dagger <odidihope@gmail.com>
2024-11-15 18:53:04 +0100

    new code new config experiementing with ts and vite

---
+++ src/components/Hero.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../styles/Hero.css";
+import heroImg from "../assets/kuda-hero-img.png";
+import appleStore from "../assets/iosStore.svg";
+import googlePlay from "../assets/googlePlay.svg";
+import cbnImg from "../assets/cbn.svg";
+import ndic from "../assets/ndic.svg";
+
+const Hero: React.FC = () => {
+  return (
+    <section className="hero-main">
+      <div className="hero">
+        <div className="hero-left">
+          <div className="hero-text">
+            <h1>The money app for Africans.</h1>
+          </div>
+
+          <div className="hero-subtext">
+            <p>
+              Make free transfers, enjoy cashless payment options and earn
+              interest on your savings with Kuda.
+            </p>
+          </div>
+
+          <div className="hero-buttons">
+            <div className="app-store">
+              <img src={appleStore} alt="ios-store" />
+            </div>
+            <div className="google-play">
+              <img src={googlePlay} alt="playstore" />
+            </div>
+          </div>
+
+          <div className="naija-info">
+            <div className="cbn">
+              <img src={cbnImg} alt="cbn" />
+            </div>
+            <div className="ndic">
+              <img src={ndic} alt="ndic" />
+            </div>
+          </div>
+        </div>
+
+        {/* Right Side of Hero Section */}
+        <div className="hero-right">
+          <img src={heroImg} alt="Kuda App Preview" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import kudaLogo from "../assets/svgexport-1.svg";
+import "../styles/Navbar.css";
+import nigeriaIcon from "../assets/nigeriaIcon.svg";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="kuda-header">
+      <div className="navbar">
+        <div className="navbar-left">
+          <div className="logo">
+            <img src={kudaLogo} alt="kuda-logo" />
+          </div>
+
+          <div className="nav-menu">
+            <div className="menu-item">
+              Personal
+              <span className="dropdown-icon ">
+                <svg width="9" height="6" viewBox="0 0 9 6" fill="none">
+                  <path d="M4.5 6L0 0H9L4.5 6Z" fill="#40196D"></path>
+                </svg>
+              </span>
+            </div>
+
+            <div className="menu-item">
+              Business
+              <span className="dropdown-icon">
+                <svg width="9" height="6" viewBox="0 0 9 6" fill="none">
+                  <path d="M4.5 6L0 0H9L4.5 6Z" fill="#40196D"></path>
+                </svg>
+              </span>
+            </div>
+
+            <div className="menu-item">
+              Company
+              <span className="dropdown-icon">
+                <svg width="9" height="6" viewBox="0 0 9 6" fill="none">
+                  <path d="M4.5 6L0 0H9L4.5 6Z" fill="#40196D"></path>
+                </svg>
+              </span>
+            </div>
+
+            <div className="menu-item">
+              Help
+              <span className="dropdown-icon">
+                <svg width="9" height="6" viewBox="0 0 9 6" fill="none">
+                  <path d="M4.5 6L0 0H9L4.5 6Z" fill="#40196D"></path>
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-right">
+          <div className="sign-in">Sign In</div>
+
+          <div className="join-kuda">Join Kuda</div>
+
+          <div className="menu-icon">
+            <img src={nigeriaIcon} alt="Nigeria Icon" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
